66bf48f9aaa7335525431573bf4f0cabb7577aeb
Prashant Kumar Ray <159636396+prashya98@users.noreply.github.com>
2025-11-03 22:29:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Select elements
+const select = document.getElementById("colorSelect");
+const button = document.querySelector('input[type="button"]');
+
+// Add event listener to button
+button.addEventListener("click", function() {
+  // Get the selected index
+  const selectedIndex = select.selectedIndex;
+
+  // If a valid option is selected, remove it
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+});
